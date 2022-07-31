421c3715fbfd8fc0b23a9577261e5861c01b8945
Sneha Deshpande <snehajoshi1895@gmail.com>
2022-07-31 14:00:53 +0530

    Correction made

--- index.js
+++ index.js
@@ -1,102 +1,113 @@
-const form = document.getElementById("form");
-const goodname = document.getElementById('name');
-const email = document.getElementById("email");
-const phone = document.getElementById("phoneNo");
-const dob = document.getElementById("dob");
-const address = document.getElementById("address");
-let validEmail = false;
-let validGoodname = false;
-let validphone = false;
-let validDOB = false;
-let validAddress = false;
-
-
  $('#failure').hide();
  $('#success').hide();
  
+            //   Validation of name
  
-
+ const goodname = document.getElementById('name');
+ let validGoodname = false;
 goodname.addEventListener('blur',()=>{
     let regex = /[A-Z][a-zA-Z\s]{1,20}$/;
     let str = goodname.value;
-    if(regex.test(str)){
-        // console.log("Your Name Is Valid");
+    let str2 = str.trim();
+    if(regex.test(str2)){
         goodname.classList.remove('is-invalid');
         validGoodname = true;
         }else{
-        // console.log("Your Name Is Not Valid");
         goodname.classList.add('is-invalid');
         validGoodname = false;
-
-    }
-   // console.log("the name is : "+validGoodname);
+     }
 })
+
+        //  Validation of address
+
+const address = document.getElementById("address");
+let validAddress = false;
    address.addEventListener('blur',()=>{
-    let regex = /[a-zA-Z0-9_\s\.\-]{1,100}$/;
+    let regex = /[a-zA-Z0-9_\s\.\-\,]{1,100}$/;
     let str = address.value;
     if(regex.test(str)){
-        // console.log("Your Name Is Valid");
-        address.classList.remove('is-invalid');
+         address.classList.remove('is-invalid');
         validAddress = true;
         }else{
-        // console.log("Your Name Is Not Valid");
-        address.classList.add('is-invalid');
+         address.classList.add('is-invalid');
         validAddress = false;
+         }
+   })
 
-    }
-   // console.log("address is : "+validAddress);
-})
+        //   Validation of email
+
+const email = document.getElementById("email");
+let validEmail = false;
 email.addEventListener('blur',()=>{
     let regex = /^[0-9a-zA-Z_\.\-]+[@][a-z]+[\.][a-z]{2,3}$/
     let str1 = email.value;
     if(regex.test(str1)){
-        // console.log("Your Email Id Is Valid");
         email.classList.remove('is-invalid');
         validEmail = true;
 
     }else{
-        // console.log("Your Email Id Is Not Valid");
-        email.classList.add('is-invalid');
+         email.classList.add('is-invalid');
         validEmail = false;
-    }
-    //console.log("email id is :"+validEmail);
-})
-phone.addEventListener('blur',()=>{
-    let regex= /^[0-9]{10}$/;
-    let str2 = phone.value;
-    if(regex.test(str2)){
-        // console.log("Your Phone Number Is Valid");
-        phone.classList.remove('is-invalid');
-        validphone = true;
-
-    }else{
-        // console.log("Your Phone Number Is Not Valid");
-        phone.classList.add('is-invalid');
-        validphone = false;
     }
     
 })
 
-dob.addEventListener('blur',()=>{
-    let regex = /^([0-9]{1,2})-([0-9]{1,2})-([0-9]{4})$/
-    let str3 = dob.value;
-    let result=moment(`${str3}`,'MM-DD-YYYY',true).isValid();
-    if(regex.test(str3) && result == true ){
+            // validation of phone number
         
-        // console.log("Your DOB Is Valid");
-        dob.classList.remove('is-invalid');
+const phone = document.getElementById("phoneNo");
+let validphone = false;
+
+phone.addEventListener('blur',()=>{
+    let regex= /^[0-9]{10}$/;
+    let str2 = phone.value;
+    if(regex.test(str2)){
+         phone.classList.remove('is-invalid');
+         validphone = true;
+     }else{
+         phone.classList.add('is-invalid');
+         validphone = false;
+    }
+    
+})
+
+            //   Validation of DOB
+
+const dob = document.getElementById("dob");
+let validDOB = false;
+dob.addEventListener('blur',()=>{
+    let regex = /^([0-9]{4})-([0-9]{2})-([0-9]{2})$/
+    let str3 = dob.value;
+    
+    if(regex.test(str3) ){
+         dob.classList.remove('is-invalid');
         validDOB = true;
 
     }else{
-        // console.log("Your DOB Is Not Valid");
-        dob.classList.add('is-invalid');
-        validDOB = false;
+         dob.classList.add('is-invalid');
+         validDOB = false;
     }
-   // console.log("email id is :"+validDOB);
+   
+})
+dob.addEventListener('focus',()=>{
+    let date = new Date();
+    let d  =date.getDate();
+    let m = date.getMonth()+1;
+    let y = date.getFullYear();
+    if(m<10){
+        m = "0" + m;
+    }
+    if(d<10){
+        d = "0" + d;
+    }
+    let today=y + "-"+ m + "-" + d;
+    document.getElementById("dob").setAttribute("max",today);
+   
+   
 })
 
+        //    Fetch API & Form validation function
 
-
+const form = document.getElementById("form");
  form.addEventListener('submit',(event) => {
     event.preventDefault();
      const formData = new FormData(form);
